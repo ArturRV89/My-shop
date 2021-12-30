185984ee61b129411d19ffb96ff1c7ea67eaf83c
Artur <artur-rvachev@rambler.ru>
2021-12-30 13:42:42 +0300

    added ['children']

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -6,7 +6,6 @@
 
 
 
-
 // Формирование запрашиваемой страницы
 // 
 // @param string $controllerName название контроллера
@@ -46,4 +45,21 @@ function d($value = null, $die = 1){
     print '</pre>';
 
     if ($die) die;
+}
+
+
+
+// Преобразование результата работы функции выборки в ассоциативный массив
+// 
+// @param recordset $rs набор строк - результат работы SELECT
+// @return array
+// 
+
+function createSmartyRsArray($rs){
+    if (!$rs) return false;
+    $smartyRs = array();
+    while ($row = mysql_fetch_assoc($rs)){
+        $smartyRs[] = $row;
+    }
+    return $smartyRs;
 }
